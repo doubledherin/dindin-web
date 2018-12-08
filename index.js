d7c21febe1dbae5130c075fc6d029b6576892eed
Wendy Dherin <wendydherin@gmail.com>
2018-12-08 13:46:54 +0000

    Initial commit.

---
+++ index.js
@@ -0,0 +1,26 @@
+'use strict'
+
+const Hapi = require('hapi')
+const server = new Hapi.Server()
+server.connection({ port: 4000 })
+
+server.bind({
+  apiBaseUrl: 'http://localhost:4000/api',
+  webBaseUrl: 'http:///localhost:4000'
+})
+
+server.register([
+  require('dindin-api'),
+  require('inert')
+], (err) => {
+
+      if (err) {
+        throw err
+      }
+
+      server.route(require('./routes'))
+
+      server.start(() => {
+        console.log('Started server at ', server.info.uri)
+      })
+})
