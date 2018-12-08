ce4bd468906e0694e9e7f5cc50a75eb5c3be70b7
Wendy Dherin <wendydherin@gmail.com>
2018-12-08 18:57:57 +0000

    Create a layout file and partials and use them.

--- index.js
+++ index.js
@@ -25,7 +25,10 @@ server.register([
         },
         relativeTo: __dirname,
         path: './views',
-        isCached: false
+        layoutPath: './views/layout',
+        layout: true,
+        isCached: false,
+        partialsPath: './views/partials'
       })
 
       server.route(require('./routes'))
