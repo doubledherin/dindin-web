800a1eddd12435a348da6fa4fb9a8acfe7182d34
Wendy Dherin <wendydherin@gmail.com>
2018-12-08 19:04:38 +0000

    Hook up homepage route to actual dindin api instead of using fake data.

--- handlers/pages.js
+++ handlers/pages.js
@@ -1,24 +1,16 @@
 'use strict'
 
+const Wreck = require('wreck')
+
 exports.home = function (request, reply) {
-  const recipes = [{
-    id: 1,
-    name: 'Silicate soup',
-    cuisine: 'Martian',
-    stars: 100,
-    serves: 1,
-    prep_time: '2 hours',
-    cooking_time: '12 minutes'
-  }, {
-    id: 2,
-    name: 'Methane trifle',
-    cuisine: 'Neptunian',
-    stars: 200,
-    serves: 1,
-    prep_time: '1 hour',
-    cooking_time: '24 minutes'
-  }]
-  reply.view('index', {
-    recipes
+  const apiUrl = this.apiBaseUrl + '/recipes'
+
+  Wreck.get(apiUrl, { json: true }, (err, res, payload) => {
+    if (err) {
+      throw err
+    }
+    reply.view('index', {
+      recipes: payload
+    })
   })
 }
