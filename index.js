a720270483b9959c39b37f0e9d965ee323480975
Wendy Dherin <wendydherin@gmail.com>
2018-12-09 18:12:10 +0000

    Create a helper to change newlines to <br> tags.

--- index.js
+++ index.js
@@ -28,7 +28,8 @@ server.register([
         layoutPath: './views/layout',
         layout: true,
         isCached: false,
-        partialsPath: './views/partials'
+        partialsPath: './views/partials',
+        helpersPath: './views/helpers'
       })
 
       server.route(require('./routes'))
